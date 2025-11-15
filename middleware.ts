de46306a80567b21fa89fb0edc5d0d95156af0fd
prabuddha-hack <projects.login03@gmail.com>
2025-11-15 09:59:14 +0530

    Implemented Clerk Auth

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const protectedRoutes = createRouteMatcher([
+  "/dashboard",
+  "/profile"
+]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRoutes(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
